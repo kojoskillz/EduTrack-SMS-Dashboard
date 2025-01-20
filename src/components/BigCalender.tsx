c51c1ee57aff22913c1297e65a580093512f39e2
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-01-20 04:31:36 +0000

    Update BigCalender.tsx

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -1,34 +1,18 @@
-"use client";
+"use client"
 
-import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
-import moment from "moment";
-import { calendarEvents } from "@/lib/data";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import { useState } from "react";
+import * as React from "react"
 
-const localizer = momentLocalizer(moment);
+import { Calendar } from "@/components/ui/calendar"
 
-const BigCalendar = () => {
-  const [view, setView] = useState<View>(Views.WORK_WEEK);
-
-  const handleOnChangeView = (selectedView: View) => {
-    setView(selectedView);
-  };
+export default function BigCalender() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
 
   return (
     <Calendar
-      localizer={localizer}
-      events={calendarEvents}
-      startAccessor="start"
-      endAccessor="end"
-      views={["work_week", "day"]}
-      view={view}
-      style={{ height: "98%" }}
-      onView={handleOnChangeView}
-      min={new Date(2025, 1, 0, 8, 0, 0)}
-      max={new Date(2025, 1, 0, 17, 0, 0)}
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md bg-red-300 border shadow"
     />
-  );
-};
-
-export default BigCalendar;
+  )
+}
