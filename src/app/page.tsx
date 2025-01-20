e6ebeb071883546e975dc6a8095fb8277deedc0d
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-01-20 13:21:02 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ import { Link } from 'react-router-dom';
 
 // Import your pages or components
 // import Homepage1 from './(dashboard)/admin/page';
-
+// import AdminPage from './(dashboard)/admin/page';
 // import About from './About';
 // import Contact from './Contact';
 
@@ -109,9 +109,9 @@ const Homepage = () => {
         </button>
 
         {/* Define your routes */}
-        <Routes>
-          <Route path="./admin" element={<Homepage />} />
-        </Routes>
+        {/* <Routes>
+          <Route path="/admin" element={<Homepage />} />
+        </Routes> */}
       </div>
     </Router>
   );
