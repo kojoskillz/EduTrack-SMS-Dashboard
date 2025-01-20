a92b35d0d4544309b4814026c54288640df0932b
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-01-20 13:04:17 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,119 @@
-const Homepage = () => {
-  return (
-    <div className=''>Welcome to Edutrack</div>
-  )
-}
+// "use client";
+// import Link from "next/link";
+// import { useForm } from "react-hook-form";
+// import { useState } from "react";
+// import { useRouter } from "next/navigation";
 
-export default Homepage
+
+// export type RegisterInputProps = {
+//   fullName: string;
+//   email: string;
+//   password: string;
+//   phone: string;
+// };
+
+// export function Homepage() {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const {
+//     register,
+//     handleSubmit,
+//     reset,
+//     formState: { errors },
+//   } = useForm<RegisterInputProps>();
+//   const router = useRouter();
+
+//   async function onSubmit(data: RegisterInputProps) {
+//     console.log(data);
+//   }
+
+//   return (
+//     <div className="w-full lg:grid h-screen lg:min-h-[600px] lg:grid-cols-2 relative ">
+//       <div className="flex items-center justify-center py-12">
+//         <div className="mx-auto grid w-[350px] gap-6">
+//           <div className="absolute top-5 left-5">Simple UI</div>
+//           <div className="grid gap-2 text-center">
+//             <h1 className="text-3xl font-bold">Create an Account</h1>
+//           </div>
+//           <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
+//             <TextInput
+//               label="Full Name"
+//               register={register}
+//               name="fullName"
+//               errors={errors}
+//               placeholder="eg John Doe"
+//             />
+//             <TextInput
+//               label="Email Address"
+//               register={register}
+//               name="email"
+//               type="email"
+//               errors={errors}
+//               placeholder="Eg. johndoe@gmail.com"
+//             />
+//             <TextInput
+//               label="Phone Number"
+//               register={register}
+//               name="phone"
+//               type="tel"
+//               errors={errors}
+//               placeholder=""
+//             />
+//             <TextInput
+//               label="Password"
+//               register={register}
+//               name="password"
+//               type="password"
+//               errors={errors}
+//               placeholder="******"
+//             />
+
+//             <SubmitButton
+//               title="Sign Up"
+//               loading={isLoading}
+//               loadingTitle="Creating Account please wait..."
+//             />
+//           </form>
+//           <div className="mt-4 text-center text-sm">
+//             Already have an account?{" "}
+//             <Link href="/login" className="underline">
+//               Login
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+    
+//     </div>
+//   );
+// }
+
+// export default Homepage;
+'use client'
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+// Import your pages or components
+import Homepage1 from './(dashboard)/admin/page';
+// import About from './About';
+// import Contact from './Contact';
+
+const Homepage = () => {
+  return ( 
+    <Router>
+      <div>
+        {/* Define links for navigation */}
+        <button>
+              <Link to="./admin">Admin</Link>
+        </button>
+
+        {/* Define your routes */}
+        <Routes>
+          <Route path="./admin" element={<admin />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default Homepage;
